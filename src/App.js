9d4fc21f5dbd75d5e62cba6dd66dd750d8d9d0c2
Almustofafadhil <almustofafadhil13@gmail.com>
2023-02-19 10:03:34 +0700

    Almustofa - Memperbaharui Struktur React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes >
+        <Route path='/' element={a}></Route>
+        <Route path='/' element={b}></Route>
+        <Route path='/' element={c}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
