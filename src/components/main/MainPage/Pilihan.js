a0faf6aab4ecb9aeed59c03ae187bb95e1fa5d5e
muhamad afif mustafa <muhamadafifmustafa83@gmail.com>
2023-02-25 16:06:56 +0700

    pull

--- src/components/main/MainPage/Pilihan.js
+++ src/components/main/MainPage/Pilihan.js
@@ -4,6 +4,7 @@ const Pilihan = () => {
     return(
         <div className="pilihanKamar">
             <a href=""><img src="./images/kamar.jpeg" width={350} className='pilihanKamar' /></a>
+            <h1>test</h1>
             <a href=""><img src="./images/kamar.jpeg" width={350} className='pilihanKamar1' /></a>
             <a href=""><img src="./images/kamar.jpeg" width={350} className='pilihanKamar2' /></a>
         </div>
