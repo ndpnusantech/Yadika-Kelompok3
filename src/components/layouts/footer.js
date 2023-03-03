ab0220891792402dd2e429a667147d5be08c139c
SitiAnih <sitianih9@gmail.com>
2023-03-03 14:07:58 +0700

    Siti anih - menambahkan footer

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -6,14 +6,20 @@ import './footer.css'
 const Footer = () => {
     return(
         <div className="footer" >
-            <a href="" className="footer1"><FaWhatsapp style={{color: 'black'}} /></a>
-            <a href="" className="footer2"><AiFillInstagram style={{color: 'black'}} /></a>
-            <a href="" className="footer3"><TfiFacebook style={{color: 'black'}} /></a>
-            <br /><br />
-            <h1>Info Support Marketing <br /><br /> Terms of Use Privacy Policy <br /><br /> © 2023 Kooheng Hotel</h1>
-            <br />
+            <p className="txt1">Info Lebih Lanjut Hubungi Marketing Kami:</p>
+            <a href="" className="icon1"><TfiFacebook/></a>
+            <a href="" className="icon2"><AiFillInstagram/></a>
+            <a href="" className="icon3"><FaWhatsapp/></a>
+            <p className="txt2"> © 2023 Kooheng Hotel</p>
         </div>
     )
 }
 
 export default Footer
+
+            {/* <a href="" className="footer1"><FaWhatsapp style={{color: 'black'}} /></a>
+            <a href="" className="footer2"><AiFillInstagram style={{color: 'black'}} /></a>
+            <a href="" className="footer3"><TfiFacebook style={{color: 'black'}} /></a>
+            <br /><br />
+            <h1>Info Support Marketing <br /><br /> Terms of Use Privacy Policy <br /><br /> © 2023 Kooheng Hotel</h1>
+            <br /> */}
