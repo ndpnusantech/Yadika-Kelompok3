cde0c3b49c5aece504c2ae383a96316fc17a86d5
muhamad afif mustafa <muhamadafifmustafa83@gmail.com>
2023-03-10 11:06:02 +0700

    muhamad Afif Mustafa - Mengubah restaurant menjadi fasilitas

---
+++ src/components/userComponents/mainSection/mainFasilitas2.js
@@ -0,0 +1,21 @@
+import "./style/mainFasilitas2.css"
+
+const MainFasilitas2 = () => {
+    return(
+        <div className="split-faci2">
+        <div className="fac1">
+            <div className="in-fac1">
+            <h1>Swimming pool</h1>
+            </div>
+            <div className="in-fac2">
+            <h4>Swimming Pool merupakan suatu rancangan bangunan berupa kolam dengan diberinya air diperuntukkan kepada masyarakat sebagai sarana tempat untuk berenang, berolahraga, serta jasa pelayanan lainnya</h4>
+            </div>
+        </div>
+        <div className="fac">
+        <img src=".\images\spa.jpg" width={450} />
+        </div>
+    </div>
+    )
+}
+
+export default MainFasilitas2
--- src/pages/pagesUser/restaurants.js
+++ src/pages/pagesUser/restaurants.js
@@ -1,12 +1,18 @@
 import Footer from "../../components/userComponents/layouts/footer"
 import Navbar from "../../components/userComponents/layouts/navbar"
-import MainRestaurants from "../../components/userComponents/mainSection/mainRestaurants"
+import MainFasilitas from "../../components/userComponents/mainSection/mainFasilitas"
+import MainFasilitas2 from "../../components/userComponents/mainSection/mainFasilitas2"
+// import MainRestaurants from "../../components/userComponents/mainSection/mainRestaurants"
 
 const Restaurants = () => {
     return(
         <div className="abo">
             <Navbar />
-            <MainRestaurants />
+            {/* <MainRestaurants /> */}
+            <MainFasilitas />
+            <MainFasilitas2 />
+            {/* <MainFasilitas />
+            <MainFasilitas2 /> */}
             <Footer />
         </div>
     )
