03ed612d34e1c9a59143df8e3941cc417e051409
Almustofafadhil <almustofafadhil13@gmail.com>
2023-03-17 10:33:16 +0700

    Almustofa - Menambahkan Halaman View

--- src/components/userComponents/pilihanView/viewStandar.js
+++ src/components/userComponents/pilihanView/viewStandar.js
@@ -5,6 +5,13 @@ const ViewStandar = () => {
     return(
     <div className="view-standar">
     <h2><FaBed /> Standar Room</h2>
+    <h1>Selamat Datang Dikamar Standar Room,</h1>
+    <h1>Disini kami menyediakan 2 views yang bagus dan cukup menarik untuk dilihat</h1>
+    <br />
+    <div className="view-standarImg">
+        <a href="./kamarStandar"><img src="./images/Pantai.webp"/></a>
+        <a href="./kamarStandar"><img src="./images/kota.jpg"/></a>
+    </div>
     </div>
     )
 }
